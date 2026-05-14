677cddb3646ddd4137058d7e9d1566759cc2bb87
Victor <vikitoree@gmail.com>
2026-05-14 16:24:56 +0300

    Add Facebook link and improve mobile UX

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -10,7 +10,11 @@ export function Footer() {
           </p>
           <div className="mt-5 flex gap-3">
             {[
-              { label: "Facebook", d: "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" },
+              {
+                label: "Facebook",
+                href: "https://www.facebook.com/GustosaSofia/",
+                d: "M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+              },
               { label: "Instagram", d: "M16 3H8a5 5 0 00-5 5v8a5 5 0 005 5h8a5 5 0 005-5V8a5 5 0 00-5-5zm-4 6a3 3 0 110 6 3 3 0 010-6zm5-1.5a1 1 0 110 2 1 1 0 010-2z" },
               { label: "Google", d: "M21 11h-9v2.9h5.2c-.2 1.5-1.7 4.3-5.2 4.3-3.1 0-5.7-2.6-5.7-5.7S8.9 6.8 12 6.8c1.8 0 3 .8 3.7 1.4l2.5-2.4C16.6 4.3 14.5 3.4 12 3.4 7.3 3.4 3.5 7.2 3.5 12s3.8 8.6 8.5 8.6c4.9 0 8.2-3.4 8.2-8.3 0-.5-.1-1-.2-1.3z" },
             ].map(s => (
